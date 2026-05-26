ba56d5b5ccc552c7cda0981454ab8d5c0b179348
yangdonghao <littlesheepdy@163.com>
2026-05-26 22:26:22 +0800

    fix: register ElementPlus globally - dialogs were not rendering

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
 
 import 'element-plus/dist/index.css'
 import './assets/main.css'
@@ -20,5 +21,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
