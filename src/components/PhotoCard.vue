c76d5d051e75a14fa12d27c516bc01ddc8c2555b
yangdonghao <littlesheepdy@163.com>
2026-06-09 22:47:47 +0800

    fix: like badge v-if 0 treated as false

--- src/components/PhotoCard.vue
+++ src/components/PhotoCard.vue
@@ -9,7 +9,7 @@
         @load="onImageLoad"
         @error="onImageError"
       />
-      <div class="like-badge" v-if="photo.likeCount">
+      <div class="like-badge" v-if="photo.likeCount != null && photo.likeCount > 0">
         <svg class="badge-heart" viewBox="0 0 24 24" fill="currentColor"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/></svg>
         {{ photo.likeCount }}
       </div>
